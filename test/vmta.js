379b9e1019054e5abed0c0504b99d714f783b62d
z.acharki <acharki.zakaria@gmail.com>
2017-04-24 22:27:34 +0100

    Basic test cases

---
+++ test/vmta.js
@@ -0,0 +1,63 @@
+// node.js built-in modules
+var assert   = require('assert');
+
+// npm modules
+var fixtures = require('haraka-test-fixtures');
+
+//Default config
+var default_config = {
+    main: {},
+    mta1: { ip: "1.1.1.1", host: "host1" },
+    mta2: { ip: "2.2.2.2", host: "host2" }
+};
+
+beforeEach(function (done) {
+    //this.outbound = new fixtures.plugin('outbound');
+    this.plugin = new fixtures.plugin('vmta');
+
+    this.connection = new fixtures.connection.createConnection();
+    this.connection.transaction = fixtures.transaction.createTransaction();
+
+    done();  // if a test hangs, assure you called done()
+});
+
+describe('VMTA plugin', function () {
+    it('loads', function (done) {
+        assert.ok(this.plugin);
+        done();
+    });
+});
+
+var next = function () {
+    test.equal(undefined, arguments[0]);
+    test.done();
+};
+
+describe('VMTA config file', function () {
+    it('loads vmta.ini from config/vmta.ini', function (done) {
+        this.plugin.load_vmta_ini();
+        assert.ok(this.plugin.cfg);
+
+        done();
+    });
+
+    it('Check availability of "x-vmta" inside Header', function (done) {
+        this.plugin.load_vmta_ini();
+
+        //console.log(this.plugin.localAddresses());
+
+        console.log();
+        assert.notDeepEqual('vmta_test', this.connection.transaction.header);
+        done();
+    });
+
+    it('Check availability of "x-vmta" inside Config file', function (done) {
+        this.plugin.load_vmta_ini();
+
+        this.connection.transaction.header.add('x-vmta', "vmta_test");
+
+        assert.deepEqual('vmta_test', default_config);
+        done();
+    });
+
+});
